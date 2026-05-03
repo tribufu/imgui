8aae4b4d66b31a2d82ea471aac9782f1bc35d619
Guilherme Werner <guilhermewerner@tribufu.com>
2026-05-03 19:05:24 -0300

    Use shared workspace target dir

--- premake5.lua
+++ premake5.lua
@@ -30,19 +30,3 @@ project "ImGui"
 
     filter "system:android"
         androidnamespace "com.tribufu.alnilam.vendor.imgui"
-
-    if _ACTION == "vs2026" and os.target() == "windows" then
-        filter { "system:windows", "architecture:x86_64" }
-            targetdir("%{prj.location}/bin/win-x64")
-        filter { "system:windows", "architecture:x86" }
-            targetdir("%{prj.location}/bin/win-x86")
-        filter { "system:windows", "architecture:AARCH64" }
-            targetdir("%{prj.location}/bin/win-arm64")
-        filter { "system:windows", "architecture:ARM" }
-            targetdir("%{prj.location}/bin/win-arm")
-    end
-
-    if not (_ACTION == "vs2026") then
-        filter { "platforms:*" }
-            targetdir("%{prj.location}/bin/%{cfg.platform}")
-    end
